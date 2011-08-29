dfeaabcbdbbc8fa7d6246a6faf3473bccb979145
Topher Brown <topher200@gmail.com>
2011-08-29 02:36:26 -0400

    scrabble: wrote add_to_board() and get_word()

--- scrabble.py
+++ scrabble.py
@@ -1,40 +1,103 @@
 from __future__ import with_statement
-import numpy
+import numpy as np
+EMPTY = '.'
+
 letters_in_hand = [
   'n', 'i', 'a', 'p', 'g', 'i', 
   ]
-global_letters = [
-  'z', 'b', 'r', 'o', 's', 'l', 'w',
-  ]
 
-def load_dictionary():
-  dictionary = []
-  with open('dictionary.txt', 'r') as f:
-    for word in f.readlines():
-      dictionary.append(word.strip())
-  return dictionary
+class Board:
+  BOARD_SIZE = 15
+  def __init__(self, ):
+    self.board =  np.zeros((self.BOARD_SIZE, self.BOARD_SIZE), 'string')
+    # TODO: find a better way to initialize to spaces
+    for x in range(len(self.board)):
+      for y in range(len(self.board[x])):
+        self.board[x,y] = EMPTY
 
-def can_make_word(possible_word, letter_pool):
-  # Check if we have a letter for each character in possible word
-  for character in possible_word:
-    if character in letter_pool:
-      # If we match, remove the letter from our letter pool
-      letter_pool.remove(character)
+  def is_blank(self, position):
+    # Try to check for blank. If we get an exception, we know we're out of bounds
+    # TODO(topher): add OOB error case
+    if self[position] == EMPTY:
+      return True
+    return False
+
+  def __getitem__(self, position):
+    return self.board[position.down, position.across]
+
+  def __setitem__(self, position, letter):
+    self.board[position.down, position.across] = letter
+
+  def __str__(self):
+    str = ''
+    for x in range(len(self.board)):
+      for y in range(len(self.board[x])):
+        str += self.board[x,y]
+      str += '\n'
+    return str
+
+# Init dictionary
+dictionary = []
+with open('dictionary.txt', 'r') as f:
+  for word in f.readlines():
+    dictionary.append(word.strip())
+
+DOWN = 0
+ACROSS = 1
+class Position():
+  def __init__(self, down, across):
+    self.down = down
+    self.across = across
+
+  def __str__(self, ):
+    return '(%i, %i)' % (self.down, self.across)
+
+  def equals (self, other_pos):
+    return ((self.down == other_pos.down) and (self.across == other_pos.across))
+
+  # TODO(topher): better way to create a copy
+  def copy(self, ):
+    return Position(self.down, self.across)
+
+board = Board()
+def add_to_board(word, starting_position, direction):
+  for i, letter in enumerate(word):
+    position = starting_position.copy()
+    if direction == ACROSS:
+      position.across += i
     else:
-      # We found a character not in our letter pool - failed!
-      return False
-  return True
+      position.down += i
+    board[position] = letter
 
-def find_all_matches(letters):
-  dictionary = load_dictionary()
-  words = []
-  for word in dictionary:
-    for letter in global_letters:
-      letters_to_check = letters + [letter]
-      if can_make_word(word, letters_to_check):
-        words.append(word)
-  return words
+def get_word(position, direction):
+  def get_before_blank(starting_position, direction, travel_direction):
+    # returns the position of the last char before a blank
+    assert(not board.is_blank(starting_position))
+    position = starting_position.copy()
+    while not board.is_blank(position):
+      if direction == ACROSS:
+        position.across += travel_direction
+      else:
+        position.down += travel_direction
+    # We have the position of the blank! Back up 1
+    if direction == ACROSS:
+      position.across -= travel_direction
+    else:
+      position.down -= travel_direction
+    return position
 
-words = set(find_all_matches(letters_in_hand))
-for word in sorted(words):
-  print word
+  start = get_before_blank(position, direction, -1)
+  end = get_before_blank(position, direction, +1)
+  word = board[start]
+  while not start.equals(end):
+    if direction == ACROSS:
+      start.across += 1
+    else:
+      start.down += 1
+    word += board[start]
+  return word
+  
+add_to_board('asdf', Position(7, 5), ACROSS)
+print(get_word(Position(7,7), ACROSS))
+
+print(board)
