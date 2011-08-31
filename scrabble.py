5f7006dd43a88a3afef77f4267b27807a796d155
Topher Brown <topher200@gmail.com>
2011-08-31 01:38:02 -0400

    scrabble: first draft of try_letters()

--- scrabble.py
+++ scrabble.py
@@ -1,10 +1,7 @@
 from __future__ import with_statement
+import itertools
 import numpy as np
 EMPTY = '.'
-
-letters_in_hand = [
-  'n', 'i', 'a', 'p', 'g', 'i', 
-  ]
 
 class Board:
   BOARD_SIZE = 15
@@ -36,6 +33,64 @@ class Board:
       str += '\n'
     return str
 
+  def copy(self):
+    new_board = Board()
+    new_board.board = self.board[:]
+    return new_board
+
+  def add_letters(self, letters, starting_position, direction):
+    position = starting_position.copy()
+    while len(letters) > 0:
+      # Put down a letter if there's nothing here
+      if self[position] == EMPTY:
+       self[position] = letters[0]
+       letters = letters[1:]
+      if direction == ACROSS:
+        position.across += 1
+      else:
+        position.down += 1
+
+  def get_word(self, position, direction):
+    def get_before_blank(starting_position, direction, travel_direction):
+      # returns the position of the last char before a blank
+      assert(not self.is_blank(starting_position))
+      position = starting_position.copy()
+      while not self.is_blank(position):
+        if direction == ACROSS:
+          position.across += travel_direction
+        else:
+          position.down += travel_direction
+      # We have the position of the blank! Back up 1
+      if direction == ACROSS:
+        position.across -= travel_direction
+      else:
+        position.down -= travel_direction
+      return position
+
+    start = get_before_blank(position, direction, -1)
+    end = get_before_blank(position, direction, +1)
+    word = self[start]
+    while not start.equals(end):
+      if direction == ACROSS:
+        start.across += 1
+      else:
+        start.down += 1
+      word += self[start]
+    return word
+
+  def try_letters(self, letters, position, direction, minimum_num_of_letters):
+    words = []
+    for num_letters in range(minimum_num_of_letters, 8):
+      for letter_combination in itertools.permutations(letters, num_letters):
+        temp_board = self.copy()
+        temp_board.add_letters(letter_combination, position, direction)
+        # TODO
+        # check if we made a word
+        #   save if we did
+        # ignores words up/down we may have made
+    return words
+  
+
 # Init dictionary
 dictionary = []
 with open('dictionary.txt', 'r') as f:
@@ -60,44 +115,8 @@ class Position():
     return Position(self.down, self.across)
 
 board = Board()
-def add_to_board(word, starting_position, direction):
-  for i, letter in enumerate(word):
-    position = starting_position.copy()
-    if direction == ACROSS:
-      position.across += i
-    else:
-      position.down += i
-    board[position] = letter
-
-def get_word(position, direction):
-  def get_before_blank(starting_position, direction, travel_direction):
-    # returns the position of the last char before a blank
-    assert(not board.is_blank(starting_position))
-    position = starting_position.copy()
-    while not board.is_blank(position):
-      if direction == ACROSS:
-        position.across += travel_direction
-      else:
-        position.down += travel_direction
-    # We have the position of the blank! Back up 1
-    if direction == ACROSS:
-      position.across -= travel_direction
-    else:
-      position.down -= travel_direction
-    return position
-
-  start = get_before_blank(position, direction, -1)
-  end = get_before_blank(position, direction, +1)
-  word = board[start]
-  while not start.equals(end):
-    if direction == ACROSS:
-      start.across += 1
-    else:
-      start.down += 1
-    word += board[start]
-  return word
-  
-add_to_board('asdf', Position(7, 5), ACROSS)
-print(get_word(Position(7,7), ACROSS))
+board.add_letters('asdf', Position(7, 5), ACROSS)
+print(board.get_word(Position(7,7), ACROSS))
+print(board.try_letters(['a', 'b',], Position(6,6), DOWN, 1))
 
 print(board)
