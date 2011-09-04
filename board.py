974263e44de50e49a69ea1a83fb3491f711d5102
Topher Brown <topher200@gmail.com>
2011-09-04 02:55:10 -0400

    initial import of board.py

---
+++ board.py
@@ -0,0 +1,91 @@
+import numpy as np
+from position import Position
+
+EMPTY_SPACE = '.'
+
+class Board:
+  BOARD_SIZE = 15
+  def __init__(self, ):
+    self.board =  np.zeros((self.BOARD_SIZE, self.BOARD_SIZE), 'string')
+    # TODO: find a better way to initialize to spaces
+    for x in range(len(self.board)):
+      for y in range(len(self.board[x])):
+        self.board[x,y] = EMPTY_SPACE
+
+  def is_blank(self, position):
+    # Returns True if the block is empty
+    if self[position] == EMPTY_SPACE:
+      return True
+    return False
+
+  def position_is_out_of_bounds(self, position):
+    return (position.down < 0 or position.down >= self.BOARD_SIZE) or \
+        (position.across < 0 or position.across >= self.BOARD_SIZE)
+
+  def __getitem__(self, position):
+    return self.board[position.down, position.across]
+
+  def __setitem__(self, position, letter):
+    self.board[position.down, position.across] = letter
+
+  def __str__(self):
+    string = ' 012345678901234\n'
+    for x in range(len(self.board)):
+      string += str(x % 10)
+      for y in range(len(self.board[x])):
+        string += self.board[x,y]
+      string += '\n'
+    return string
+
+  def copy(self):
+    new_board = Board()
+    new_board.board = self.board.copy()
+    return new_board
+
+  def add_letters(self, letters, starting_position, direction):
+    '''Put letters on each blank space on the board, starting at
+    starting_position and moving in direction until all the letters are used
+    up.'''
+    position = starting_position.copy()
+    while len(letters) > 0:
+      if self.position_is_out_of_bounds(position):
+        raise OutOfBoundsException("trying to add a letter OOB")
+      if self.is_blank(position):
+        # There's nothing here- put a letter down
+        self[position] = letters[0]
+        letters = letters[1:]
+      # Move to the next position
+      position.add_in_direction(1, direction)
+
+  def get_word(self, position, direction):
+    def get_before_blank(starting_position, direction, travel_direction):
+      '''Returns the position of the last char before a blank (or edge of
+      board). Moves along direction (DOWN, ACROSS) in travel_direction
+      (left/up/-1 or down/across/+1).'''
+      assert(not self.is_blank(starting_position))
+      position = starting_position.copy()
+      while not (self.position_is_out_of_bounds(position) or
+                 self.is_blank(position)):
+        position.add_in_direction(travel_direction, direction)
+      # We have the position of the blank/edge! Back up 1.
+      position.add_in_direction(-travel_direction, direction)
+      return position
+
+    position = get_before_blank(position, direction, -1)
+    end = get_before_blank(position, direction, +1)
+    word = self[position]
+    while not position.equals(end):
+      position.add_in_direction(1, direction)
+      word += self[position]
+    return word
+
+  def get_position_of_letters_on_board(self,):
+    '''Returns a Position for each letter currently placed on the board'''
+    position_list = []
+    for x in range(len(self.board)):
+      for y in range(len(self.board[0])):
+        position = Position(x, y)
+        if not self.is_blank(position):
+          position_list.append(position)
+    return position_list
+
--- scrabble.py
+++ scrabble.py
@@ -1,10 +1,9 @@
 from __future__ import division
 from __future__ import with_statement
 import itertools
-import numpy as np
 import time
 from position import Position
-EMPTY_SPACE = '.'
+from board import Board
 
 # Init dictionary
 DICTIONARY = []
@@ -15,81 +14,9 @@ with open('short_dictionary.txt', 'r') as f:
 class OutOfBoundsException(Exception):
   pass
 
-class Board:
-  BOARD_SIZE = 15
-  def __init__(self, ):
-    self.board =  np.zeros((self.BOARD_SIZE, self.BOARD_SIZE), 'string')
-    # TODO: find a better way to initialize to spaces
-    for x in range(len(self.board)):
-      for y in range(len(self.board[x])):
-        self.board[x,y] = EMPTY_SPACE
-
-  def is_blank(self, position):
-    # Returns True if the block is empty
-    if self[position] == EMPTY_SPACE:
-      return True
-    return False
-
-  def position_is_out_of_bounds(self, position):
-    return (position.down < 0 or position.down >= self.BOARD_SIZE) or \
-        (position.across < 0 or position.across >= self.BOARD_SIZE)
-
-  def __getitem__(self, position):
-    return self.board[position.down, position.across]
-
-  def __setitem__(self, position, letter):
-    self.board[position.down, position.across] = letter
-
-  def __str__(self):
-    string = ' 012345678901234\n'
-    for x in range(len(self.board)):
-      string += str(x % 10)
-      for y in range(len(self.board[x])):
-        string += self.board[x,y]
-      string += '\n'
-    return string
-
-  def copy(self):
-    new_board = Board()
-    new_board.board = self.board.copy()
-    return new_board
-
-  def add_letters(self, letters, starting_position, direction):
-    '''Put letters on each blank space on the board, starting at
-    starting_position and moving in direction until all the letters are used
-    up.'''
-    position = starting_position.copy()
-    while len(letters) > 0:
-      if self.position_is_out_of_bounds(position):
-        raise OutOfBoundsException("trying to add a letter OOB")
-      if self.is_blank(position):
-        # There's nothing here- put a letter down
-        self[position] = letters[0]
-        letters = letters[1:]
-      # Move to the next position
-      position.add_in_direction(1, direction)
-
-  def get_word(self, position, direction):
-    def get_before_blank(starting_position, direction, travel_direction):
-      '''Returns the position of the last char before a blank (or edge of
-      board). Moves along direction (DOWN, ACROSS) in travel_direction
-      (left/up/-1 or down/across/+1).'''
-      assert(not self.is_blank(starting_position))
-      position = starting_position.copy()
-      while not (self.position_is_out_of_bounds(position) or
-                 self.is_blank(position)):
-        position.add_in_direction(travel_direction, direction)
-      # We have the position of the blank/edge! Back up 1.
-      position.add_in_direction(-travel_direction, direction)
-      return position
-
-    position = get_before_blank(position, direction, -1)
-    end = get_before_blank(position, direction, +1)
-    word = self[position]
-    while not position.equals(end):
-      position.add_in_direction(1, direction)
-      word += self[position]
-    return word
+class Scrabble:
+  def __init__(self, board):
+    self.board = board
 
   def get_position_of_letters_on_board(self,):
     '''Returns a Position for each letter currently placed on the board'''
@@ -97,7 +24,7 @@ class Board:
     for x in range(len(self.board)):
       for y in range(len(self.board[0])):
         position = Position(x, y)
-        if not self.is_blank(position):
+        if not self.board.is_blank(position):
           position_list.append(position)
     return position_list
 
@@ -109,7 +36,7 @@ class Board:
     for num_letters in range(minimum_num_of_letters, 8):
       for potential_word in itertools.permutations(letters, num_letters):
         # Make a fake board and add these letters to it
-        temp_board = self.copy()
+        temp_board = self.board.copy()
         try:
           temp_board.add_letters(potential_word, position, direction)
         except OutOfBoundsException:
@@ -135,10 +62,10 @@ class Board:
         for distance_away_from_position in magnitudes_to_try:
           position = base_position.copy()
           position.add_in_direction(distance_away_from_position, direction)
-          if self.position_is_out_of_bounds(position):
+          if self.board.position_is_out_of_bounds(position):
             # Can't start at an out of bounds position
             continue
-          if not self.is_blank(position):
+          if not self.board.is_blank(position):
             # Skipping position- already has a letter
             continue
           print 'position: %s' % position
@@ -162,13 +89,14 @@ def main():
   start_time = time.time()
 
   board = Board()
-  board.add_letters('radar', Position(7, 4), Position.ACROSS)
-  board.add_letters('oom', Position(8, 4), Position.DOWN)
-  board.add_letters('eet', Position(10, 5), Position.ACROSS)
-  board.add_letters('admie', Position(3, 8), Position.DOWN)
-  print(board)
+  game = Scrabble(board)
+  game.board.add_letters('radar', Position(7, 4), Position.ACROSS)
+  game.board.add_letters('oom', Position(8, 4), Position.DOWN)
+  game.board.add_letters('eet', Position(10, 5), Position.ACROSS)
+  game.board.add_letters('admie', Position(3, 8), Position.DOWN)
+  print(game.board)
 
-  print(board.get_possible_words([
+  print(game.get_possible_words([
         't', 'e', 'c', 
         ]))
 
