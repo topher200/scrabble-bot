be5d9ee1cdff1bb2320983a357904a86f1d81444
Topher Brown <topher200@gmail.com>
2011-09-08 00:43:00 -0400

    map.py: initial import

---
+++ map.py
@@ -0,0 +1,32 @@
+import logging
+import time
+import sys
+from position import Position, PositionToTry
+from scrabble import Scrabble
+
+def set_up_game():
+  game = Scrabble()
+
+  game.board.add_letters('radar', Position(7, 4), Position.ACROSS)
+  game.board.add_letters('oom', Position(8, 4), Position.DOWN)
+  game.board.add_letters('eet', Position(10, 5), Position.ACROSS)
+  game.board.add_letters('admie', Position(3, 8), Position.DOWN)
+
+  return game
+
+def main(args):
+  start_time = time.time()
+
+  game = set_up_game()
+  position_to_try = PositionToTry()
+  position_to_try.parse_from_string(args[1])
+
+  print(game.try_letters_at_position([
+        't', 'e', 'c', 
+        ], position_to_try))
+
+  end_time = time.time()
+  logging.debug("script took %s minutes" % ((end_time - start_time) / 60))
+
+if __name__ == '__main__':
+  main(sys.argv)
