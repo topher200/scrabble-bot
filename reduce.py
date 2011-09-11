5386f4e2c4127bcd2b31ea44c5d84754a8282f1d
Topher Brown <topher200@gmail.com>
2011-09-11 12:56:49 -0400

    reduce.py: initial import

---
+++ reduce.py
@@ -0,0 +1,17 @@
+import sys
+
+def main(args):
+  words = set()
+  for word_list in args[1]:
+    if type(word_list) == str:
+      # Crap- it's not a list. It's just a single word
+      words.add(word_list)
+    else:
+      assert type(word_list) == list
+      # Add every word in the list
+      for word in word_list:
+        words.add(word)
+  print words
+
+if __name__ == '__main__':
+  main(sys.argv)
