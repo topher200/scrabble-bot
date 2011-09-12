f7a324a1103ec235d0af7dec46c6d62c0384a02b
Topher Brown <topher200@gmail.com>
2011-09-12 00:24:52 -0400

    classes.tar -> helper_classes.tar

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,5 +2,5 @@ s3cmd sync input.txt s3://tophernet.scrabble
 s3cmd sync short_dictionary.txt s3://tophernet.scrabble
 s3cmd sync dictionary.txt s3://tophernet.scrabble
 s3cmd sync *.py s3://tophernet.scrabble
-tar -cf classes.tar classes/*
-s3cmd sync classes.tar s3://tophernet.scrabble
+tar -cf helper_classes.tar classes/*
+s3cmd sync helper_classes.tar s3://tophernet.scrabble
