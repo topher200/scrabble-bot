fe6417401607daa8e8fc3098227bd3cfb5f3fc20
Topher Brown <topher200@gmail.com>
2011-09-12 00:11:36 -0400

    add_streaming: nit

--- add_streaming.sh
+++ add_streaming.sh
@@ -1,2 +1,8 @@
 ./upload_to_s3.sh
-elastic-mapreduce --stream --mapper s3://tophernet.scrabble/map.py --input s3://tophernet.scrabble/input.txt --output s3://tophernet.scrabble.output/out_1 --reducer s3://tophernet.scrabble/reduce.py  -j j-3BASVGYS0MIX8
+
+elastic-mapreduce --stream \
+--mapper s3://tophernet.scrabble/map.py \
+--input s3://tophernet.scrabble/input.txt \
+--output s3://tophernet.scrabble.output/out_1 \
+--reducer aggregate  \
+-j j-3BASVGYS0MIX8
