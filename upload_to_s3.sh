8153dbd0ddb51a4223e3f923b125bf523290100f
Topher Brown <topher200@gmail.com>
2011-09-12 02:03:19 -0400

    upload_to_s3: making tar from selected helper classes (not dir)

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,5 +2,5 @@ s3cmd sync input.txt s3://tophernet.scrabble
 s3cmd sync short_dictionary.txt s3://tophernet.scrabble
 s3cmd sync dictionary.txt s3://tophernet.scrabble
 s3cmd sync *.py s3://tophernet.scrabble
-tar -cf helper_classes.tar helper_classes/*
+tar -cf helper_classes.tar position.py scrabble.py board.py
 s3cmd sync helper_classes.tar s3://tophernet.scrabble
