7d45227caadb994786fe24f2c0337b1c58d05982
Topher Brown <topher200@gmail.com>
2011-09-12 01:02:38 -0400

    add_streaming: using the datetime to name the output directory

--- add_streaming.sh
+++ add_streaming.sh
@@ -1,11 +1,10 @@
 ./upload_to_s3.sh
 
-s3cmd del -r s3://tophernet.scrabble.output/output
-
+datetime=$(date +"%Y%m%d-%T")
 elastic-mapreduce --stream \
 --mapper s3://tophernet.scrabble/map.py \
 --input s3://tophernet.scrabble/input.txt \
---output s3://tophernet.scrabble.output/output \
+--output s3://tophernet.scrabble.output/output_$datetime \
 --reducer aggregate  \
 --cache-archive s3://tophernet.scrabble/helper_classes.tar#helper_classes \
 -j j-3BASVGYS0MIX8
