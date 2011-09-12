7c1c5cc05cb3fb52ddb0b458c017a2542882882f
Topher Brown <topher200@gmail.com>
2011-09-11 23:28:37 -0400

    add_streaming: running upload script before starting job

--- add_streaming.sh
+++ add_streaming.sh
@@ -1,1 +1,2 @@
-elastic-mapreduce --stream --mapper s3://tophernet.scrabble/map.py --input s3://tophernet.scrabble/input.txt --output s3://tophernet.scrabble.output/out_0 --reducer s3://tophernet.scrabble/reduce.py  -j j-687MC5JWXSDT
+./upload_to_s3.sh
+elastic-mapreduce --stream --mapper s3://tophernet.scrabble/map.py --input s3://tophernet.scrabble/input.txt --output s3://tophernet.scrabble.output/out_1 --reducer s3://tophernet.scrabble/reduce.py  -j j-3BASVGYS0MIX8
