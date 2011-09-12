c1ba283a7c46c2e6b817bcda2fd22d3994032436
Topher Brown <topher200@gmail.com>
2011-09-12 00:47:44 -0400

    add_streaming: new output dir

--- add_streaming.sh
+++ add_streaming.sh
@@ -3,7 +3,7 @@
 elastic-mapreduce --stream \
 --mapper s3://tophernet.scrabble/map.py \
 --input s3://tophernet.scrabble/input.txt \
---output s3://tophernet.scrabble.output/out_1 \
+--output s3://tophernet.scrabble.output/output \
 --reducer aggregate  \
 --cache-archive s3://tophernet.scrabble/helper_classes.tar#helper_classes \
 -j j-3BASVGYS0MIX8
