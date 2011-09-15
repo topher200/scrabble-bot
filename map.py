ebfe9e3bacfae7f919dd6f75fafc3c4a42c5c31c
Topher Brown <topher200@gmail.com>
2011-09-14 23:56:26 -0400

    map: continuing game

--- map.py
+++ map.py
@@ -9,10 +9,13 @@ from scrabble import Scrabble
 def set_up_game():
   game = Scrabble()
 
-  game.board.add_letters('radar', Position(7, 4), Position.ACROSS)
-  game.board.add_letters('oom', Position(8, 4), Position.DOWN)
-  game.board.add_letters('eet', Position(10, 5), Position.ACROSS)
-  game.board.add_letters('admie', Position(3, 8), Position.DOWN)
+  game.board.add_letters('sire', Position(7, 7), Position.ACROSS)
+  game.board.add_letters('peheats', Position(6, 9), Position.DOWN)
+  game.board.add_letters('jt', Position(10, 8), Position.ACROSS)
+  game.board.add_letters('sidd', Position(8, 6), Position.ACROSS)
+  game.board.add_letters('gri', Position(12, 6), Position.ACROSS)
+  game.board.add_letters('ba', Position(10, 6), Position.DOWN)
+  game.board.add_letters('ty', Position(10, 11), Position.ACROSS)
 
   return game
 
