ee5df2fb99d66f86173aea1118c08d67e325e9fc
Topher Brown <topher200@gmail.com>
2011-10-04 01:55:22 -0400

    generate_input_file: getting game from new spot in scrabble.py

--- generate_input_file.py
+++ generate_input_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from __future__ import with_statement
-import map
+import scrabble
 
-game = map.set_up_game()
+game = scrabble.set_up_game()
 filename = 'positions_to_try.txt'
 with open(filename, 'w') as file:
   for ptt in game.generate_positions_to_try():
