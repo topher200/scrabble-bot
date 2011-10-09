a45ebf77f2cea3cc3820563789e51fd6ed5c77ac
Topher Brown <topher200@gmail.com>
2011-10-09 02:40:14 -0400

    upload_to_s3: we're using the large dictionary now

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -2,5 +2,5 @@ s3cmd sync positions_to_try.txt s3://tophernet.scrabble
 s3cmd sync map.py s3://tophernet.scrabble
 s3cmd sync reduce.py s3://tophernet.scrabble
 tar -cf helper_classes.tar position.py scrabble.py board.py __init__.py \
-    short_dictionary.txt future_itertools.py
+    dictionary.txt future_itertools.py
 s3cmd sync helper_classes.tar s3://tophernet.scrabble
