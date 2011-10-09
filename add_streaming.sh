fa8594ac7134fc20f10a5c91c4017fc8e9bd4581
Topher Brown <topher200@gmail.com>
2011-10-09 04:00:12 -0400

    updating helper functions with new directory structure

--- add_streaming.sh
+++ add_streaming.sh
@@ -7,5 +7,5 @@ elastic-mapreduce --stream \
 --input s3://tophernet.scrabble/positions_to_try.txt \
 --output s3://tophernet.scrabble.output/output_$datetime \
 --reducer s3://tophernet.scrabble/reduce.py  \
---cache-archive s3://tophernet.scrabble/helper_classes.tar#helper_classes \
+--cache-archive s3://tophernet.scrabble/scrabble.tar#scrabble \
 -j j-1KN0GPY19L722
