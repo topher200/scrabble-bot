1d7390be22cc894be060ba7abe81f69756dbe91a
Topher Brown <topher200@gmail.com>
2011-10-09 05:18:59 -0400

    upload_to_s3: grabbing only the files we want for our tar

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,5 +1,6 @@
 s3cmd sync positions_to_try.txt s3://tophernet.scrabble
 s3cmd sync map.py s3://tophernet.scrabble
 s3cmd sync reduce.py s3://tophernet.scrabble
-tar -cf scrabble.tar scrabble/*
+find scrabble | grep -e txt -e py$ | awk -F '/' '{print $2}' | \
+    xargs tar -C scrabble -cf scrabble.tar
 s3cmd sync scrabble.tar s3://tophernet.scrabble
